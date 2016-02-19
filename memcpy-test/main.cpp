0b06f561284012b938be9f5ba188ab329bc64860
Oleg Andreev <olegoandreev@yandex.ru>
2016-02-19 12:41:16 +0300

    memcpy-test: A sample for testing memcpy performance.

---
+++ memcpy-test/main.cpp
@@ -0,0 +1,144 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#if defined(__APPLE__)
+#include <sys/time.h>
+#elif defined(__linux__)
+#include <time.h>
+#endif
+
+void libcMemcpy(void* dst, const void* src, size_t size);
+void naiveMemcpy(void* dst, const void* src, size_t size);
+void naiveSseMemcpy(void* dst, const void* src, size_t size);
+void unrolled2xSseMemcpy(void* dst, const void* src, size_t size);
+void unrolled4xSseMemcpy(void* dst, const void* src, size_t size);
+// void muslMemcpy(void* dst, const void* src, size_t size);
+
+// Guaranteed to get into L1.
+const int L1_SIZE = 16 * 1024;
+// Guaranteed to get into L2 but not in L1.
+const int L2_SIZE = 96 * 1024;
+// Guaranteed to get into main memory.
+const int MAIN_SIZE = 64 * 1024 * 1024;
+
+int64_t getTimeCounter()
+{
+#if defined(__APPLE__)
+    timeval tp;
+    gettimeofday(&tp, nullptr);
+    return tp.tv_sec * 1000000ULL + tp.tv_usec;
+#elif defined(__linux__)
+    timespec tp;
+    clock_gettime(CLOCK_MONOTONIC, &tp);
+    return tp.tv_sec * 1000000000ULL + tp.tv_nsec;
+#endif
+}
+
+int64_t getTimeFreq()
+{
+#if defined(__APPLE__)
+    return 1000000;
+#elif defined(__linux__)
+    return 1000000000;
+#endif
+}
+
+// Take strides of size strideSize from one half of the block and randomly copy them to the other half.
+template <typename T>
+size_t shuffleBlock(char* block, size_t blockSize, size_t strideSize, const T& memcpyFunc)
+{
+    size_t numStrides = blockSize / strideSize;
+    char* fromBase;
+    char* toBase;
+    if (rand() % 2 == 0) {
+        fromBase = block;
+        toBase = block + blockSize / 2;
+    } else {
+        fromBase = block + blockSize / 2;
+        toBase = block;
+    }
+
+    for (size_t from = 0; from < numStrides / 2; from++) {
+        size_t to = rand() % (numStrides / 2);
+        memcpyFunc(toBase + to * strideSize, fromBase + from * strideSize, strideSize);
+    }
+
+    return blockSize / 2;
+}
+
+template <typename T>
+void testMemcpy(char* block, size_t blockSize, size_t strideSize, const T& memcpyFunc, const char* memcpyName)
+{
+    int64_t timeFreq = getTimeFreq();
+    int64_t totalBytes = 0;
+    size_t numIters = 0;
+    int64_t start = getTimeCounter();
+    int64_t deltaUsec;
+    while (true) {
+        totalBytes += shuffleBlock(block, blockSize, strideSize, memcpyFunc);
+        numIters++;
+        deltaUsec = getTimeCounter() - start;
+        if (deltaUsec > timeFreq) {
+            break;
+        }
+    }
+    double gbPerSec = double(totalBytes) * timeFreq / (deltaUsec * 1024 * 1024 * 1024);
+    if (gbPerSec > 10) {
+        printf("%s: block size %d with copy stride %d: %.1f Gb/sec\n", memcpyName, (int)blockSize, (int)strideSize, gbPerSec);
+    } else {
+        printf("%s: block size %d with copy stride %d: %.2f Gb/sec\n", memcpyName, (int)blockSize, (int)strideSize, gbPerSec);
+    }
+}
+
+template <typename T, size_t N>
+size_t arraySize(T(&)[N])
+{
+    return N;
+}
+
+int main()
+{
+    srand(0);
+    
+    char* block = new char[MAIN_SIZE];
+    for (size_t i = 0; i < MAIN_SIZE; i++) {
+        block[i] = i;
+    }
+
+    // const size_t L1_STRIDES[] = { 8, 16, 128, L1_SIZE / 4, L1_SIZE / 2 };
+     const size_t L1_STRIDES[] = { 16, 128, L1_SIZE / 4, L1_SIZE / 2 };
+    for (size_t i = 0; i < arraySize(L1_STRIDES); i++) {
+        testMemcpy(block, L1_SIZE, L1_STRIDES[i], libcMemcpy, "libc");
+        testMemcpy(block, L1_SIZE, L1_STRIDES[i], naiveMemcpy, "naive");
+        testMemcpy(block, L1_SIZE, L1_STRIDES[i], naiveSseMemcpy, "naiveSse");
+        testMemcpy(block, L1_SIZE, L1_STRIDES[i], unrolled2xSseMemcpy, "unrolled2xSseMemcpy");
+        testMemcpy(block, L1_SIZE, L1_STRIDES[i], unrolled4xSseMemcpy, "unrolled4xSseMemcpy");
+        printf("\n");
+    }
+
+    const size_t L2_STRIDES[] = { 8, 16, 128, L1_SIZE / 2, L2_SIZE / 4, L2_SIZE / 2 };
+    for (size_t i = 0; i < arraySize(L2_STRIDES); i++) {
+        testMemcpy(block, L2_SIZE, L2_STRIDES[i], libcMemcpy, "libc");
+        testMemcpy(block, L2_SIZE, L2_STRIDES[i], naiveMemcpy, "naive");
+        testMemcpy(block, L2_SIZE, L2_STRIDES[i], naiveSseMemcpy, "naiveSse");
+        testMemcpy(block, L2_SIZE, L2_STRIDES[i], unrolled2xSseMemcpy, "unrolled2xSseMemcpy");
+        testMemcpy(block, L2_SIZE, L2_STRIDES[i], unrolled4xSseMemcpy, "unrolled4xSseMemcpy");
+        printf("\n");
+    }
+
+    const size_t MAIN_STRIDES[] = { 8, 16, 128, L1_SIZE / 2, L2_SIZE / 2, MAIN_SIZE / 4, MAIN_SIZE / 2 };
+    for (size_t i = 0; i < arraySize(MAIN_STRIDES); i++) {
+        testMemcpy(block, MAIN_SIZE, MAIN_STRIDES[i], libcMemcpy, "libc");
+        testMemcpy(block, MAIN_SIZE, MAIN_STRIDES[i], naiveMemcpy, "naive");
+        testMemcpy(block, MAIN_SIZE, MAIN_STRIDES[i], naiveSseMemcpy, "naiveSse");
+        testMemcpy(block, MAIN_SIZE, MAIN_STRIDES[i], unrolled2xSseMemcpy, "unrolled2xSseMemcpy");
+        testMemcpy(block, MAIN_SIZE, MAIN_STRIDES[i], unrolled4xSseMemcpy, "unrolled4xSseMemcpy");
+        printf("\n");
+    }
+    
+    int code = 0;
+    for (size_t i = 0; i < MAIN_SIZE; i++) {
+        code += block[i];
+    }
+    return code;
+}
---
+++ memcpy-test/memcpy-test.cpp
@@ -0,0 +1,225 @@
+#include <stdint.h>
+#include <string.h>
+#include <xmmintrin.h>
+
+// Fucking strict aliasing.
+uint64_t u64_load(const char* src)
+{
+    uint64_t ret;
+    memcpy(&ret, src, sizeof(ret));
+    return ret;
+}
+
+void u64_store(uint64_t v, char* dst)
+{
+    memcpy(dst, &v, sizeof(v));
+}
+
+void libcMemcpy(void* dst, const void* src, size_t size)
+{
+    memmove(dst, src, size);
+}
+
+void naiveMemcpy(void* dst, const void* src, size_t size)
+{
+    char* cdst = (char*)dst;
+    const char* csrc = (char*)src;
+    const char* end = csrc + size;
+    const char* aend = end - size % 8;
+    while (csrc != aend) {
+        u64_store(u64_load(csrc), cdst);
+        csrc += 8;
+        cdst += 8;
+    }
+    while (csrc != end) {
+        *cdst = *csrc;
+        csrc++;
+        cdst++;
+    }
+}
+
+template <size_t align, typename T>
+T* alignedPtr(T* p)
+{
+    return (T*)(((uintptr_t)p + (align - 1)) & ~(align - 1));
+}
+
+template <size_t align, typename T>
+const T* alignedPtr(const T* p)
+{
+    return (const T*)(((uintptr_t)p + (align - 1)) & ~(align - 1));
+}
+
+template <size_t align, typename T>
+const T* alignedEnd(const T* p)
+{
+    return (const T*)((uintptr_t)p & ~(align - 1));
+}
+
+template <size_t align, typename T>
+bool isAligned(const T* p)
+{
+    return (((uintptr_t)p) & (align - 1)) == 0;
+}
+
+void naiveSseMemcpy(void* dst, const void* src, size_t size)
+{
+    if (size < 16) {
+        naiveMemcpy(dst, src, size);
+        return;
+    }
+
+    char* cdst = (char*)dst;
+    const char* csrc = (char*)src;
+    const char* end = csrc + size;
+    const char* abegin = alignedPtr<16>(csrc);
+    const char* aend = alignedEnd<16>(end);
+
+    while (csrc != abegin) {
+        *cdst = *csrc;
+        csrc++;
+        cdst++;
+    }
+    if (isAligned<16>(cdst)) {
+        while (csrc != aend) {
+            _mm_store_ps((float*)cdst, _mm_load_ps((float*)csrc));
+            csrc += 16;
+            cdst += 16;
+        }
+    } else {
+        while (csrc != aend) {
+            _mm_storeu_ps((float*)cdst, _mm_load_ps((float*)csrc));
+            csrc += 16;
+            cdst += 16;
+        }
+    }
+    while (csrc != end) {
+        *cdst = *csrc;
+        csrc++;
+        cdst++;
+    }
+}
+
+void unrolled2xSseMemcpy(void* dst, const void* src, size_t size)
+{
+    if (size < 32) {
+        naiveMemcpy(dst, src, size);
+        return;
+    }
+
+    char* cdst = (char*)dst;
+    const char* csrc = (char*)src;
+    const char* end = csrc + size;
+    const char* abegin = alignedPtr<32>(csrc);
+    const char* aend = alignedEnd<32>(end);
+
+    while (csrc != abegin) {
+        *cdst = *csrc;
+        csrc++;
+        cdst++;
+    }
+    if (isAligned<16>(cdst)) {
+        while (csrc != aend) {
+            _mm_store_ps((float*)cdst, _mm_load_ps((float*)csrc));
+            csrc += 16;
+            cdst += 16;
+            _mm_store_ps((float*)cdst, _mm_load_ps((float*)csrc));
+            csrc += 16;
+            cdst += 16;
+        }
+    } else {
+        while (csrc != aend) {
+            _mm_storeu_ps((float*)cdst, _mm_load_ps((float*)csrc));
+            csrc += 16;
+            cdst += 16;
+            _mm_storeu_ps((float*)cdst, _mm_load_ps((float*)csrc));
+            csrc += 16;
+            cdst += 16;
+        }
+    }
+    while (csrc != end) {
+        *cdst = *csrc;
+        csrc++;
+        cdst++;
+    }
+}
+
+void unrolled4xSseMemcpy(void* dst, const void* src, size_t size)
+{
+    if (size < 64) {
+        naiveMemcpy(dst, src, size);
+        return;
+    }
+
+    char* cdst = (char*)dst;
+    const char* csrc = (char*)src;
+    const char* end = csrc + size;
+    const char* abegin = alignedPtr<64>(csrc);
+    const char* aend = alignedEnd<64>(end);
+
+    while (csrc != abegin) {
+        *cdst = *csrc;
+        csrc++;
+        cdst++;
+    }
+    if (isAligned<16>(cdst)) {
+        while (csrc != aend) {
+            _mm_store_ps((float*)cdst, _mm_load_ps((float*)csrc));
+            csrc += 16;
+            cdst += 16;
+            _mm_store_ps((float*)cdst, _mm_load_ps((float*)csrc));
+            csrc += 16;
+            cdst += 16;
+            _mm_store_ps((float*)cdst, _mm_load_ps((float*)csrc));
+            csrc += 16;
+            cdst += 16;
+            _mm_store_ps((float*)cdst, _mm_load_ps((float*)csrc));
+            csrc += 16;
+            cdst += 16;
+        }
+    } else {
+        while (csrc != aend) {
+            _mm_storeu_ps((float*)cdst, _mm_load_ps((float*)csrc));
+            csrc += 16;
+            cdst += 16;
+            _mm_storeu_ps((float*)cdst, _mm_load_ps((float*)csrc));
+            csrc += 16;
+            cdst += 16;
+            _mm_storeu_ps((float*)cdst, _mm_load_ps((float*)csrc));
+            csrc += 16;
+            cdst += 16;
+            _mm_storeu_ps((float*)cdst, _mm_load_ps((float*)csrc));
+            csrc += 16;
+            cdst += 16;
+        }
+    }
+    while (csrc != end) {
+        *cdst = *csrc;
+        csrc++;
+        cdst++;
+    }
+}
+
+// void muslMemcpy(void* dst, const void* src, size_t size)
+// {
+//     asm("movq %%rdi, %%rax\n"
+//         "cmpq $8, %%rdx\n"
+//         "jc 1f\n"
+//         "testl $7, %%edi\n"
+//         "jz 1f\n"
+// "2:      movsb\n"
+//         "decq %%rdx\n"
+//         "testl $7, %%edi\n"
+//         "jnz 2b\n"
+// "1:      movq %%rdx, %%rcx\n"
+//         "shrq $3, %%rcx\n"
+//         "rep movsq\n"
+//         "andl $7, %%edx\n"
+//         "jz 1f\n"
+// "2:      movsb\n"
+//         "decl %%edx\n"
+//         "jnz 2b\n"
+//     :
+//     :
+//     );
+// }
