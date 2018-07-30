01fa0b40e35e326d39495ecb66969821a338628f
Oleg Andreev <olegoandreev@yandex.ru>
2018-07-30 23:10:43 +0300

    common: Added the NO_INLINE to match the FORCE_INLINE.

--- common.h
+++ common.h
@@ -60,10 +60,13 @@ static_assert(sizeof(size_t) == sizeof(uintptr_t), "Very strange platform");
 //
 #if defined(__clang__) || defined(__GNUC__)
 #define FORCE_INLINE __attribute__((always_inline)) inline
+#define NO_INLINE __attribute__((noinline))
 #elif defined(_MSC_VER)
 #define FORCE_INLINE __forceinline
+#define NO_INLINE __declspec(noinline)
 #else
 #define FORCE_INLINE
+#define NO_INLINE
 #endif
 
 //
