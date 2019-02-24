53c167f6b90151f858ce8123479beddddac1a12a
olegoandreev <olegoandreev@yandex.ru>
2019-02-24 23:08:28 +0300

    threadpool: Fixed CountWaiter for the case of multiple parallel waiters.

--- threadpool/countwaiter.h
+++ threadpool/countwaiter.h
@@ -4,7 +4,7 @@
 
 #include "common.h"
 
-// A structure for waiting until the count becomes zero (a la CountDownLatch in Java).
+// A synchronization primitive for waiting until the target count value is reached (like CountDownLatch in Java).
 class CountWaiter {
 public:
     CountWaiter(int targetCount)
@@ -12,22 +12,47 @@ public:
     {
     }
 
-    void post(int count = 1)
+    ~CountWaiter()
     {
-        // We can do a relaxed here, because a semaphore post-wait pair SHOULD be a rel-acq pair.
-        if (counter.fetch_sub(count, std::memory_order_acq_rel) <= count) {
-            semaphore.post();
-        }
     }
 
+    // Posts (i.e. increases) the count value by the given amount. Returns true if the waiter have been awoken
+    // by this post.
+    bool post(int count = 1)
+    {
+        if (counter.fetch_sub(count, std::memory_order_acq_rel) > count) {
+            return false;
+        }
+
+        int numWakeup = numWaiters.load(std::memory_order_relaxed);
+
+        for (int i = 0; i < numWakeup; i++) {
+            semaphore.post();
+        }
+
+        return true;
+    }
+
+    // Waits until the target count value has been hit.
     void wait()
     {
+        if (counter.load(std::memory_order_acquire) <= 0) {
+            return;
+        }
+
+        numWaiters.fetch_add(1, std::memory_order_relaxed);
+
+        // Recheck that the condition has not been met.
         if (counter.load(std::memory_order_acquire) > 0) {
             semaphore.wait();
         }
+
+        numWaiters.fetch_sub(1, std::memory_order_relaxed);
     }
 
 private:
+    // counter is decreasing from targetCount to zero.
     std::atomic<int> counter;
+    std::atomic<int> numWaiters{0};
     Semaphore semaphore;
 };
