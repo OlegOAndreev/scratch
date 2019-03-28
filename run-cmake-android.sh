4259a499512e9738ad863a8306205303df61a69f
olegoandreev <olegoandreev@yandex.ru>
2019-03-28 17:17:11 +0300

    Fixed run-cmake-android.sh script.

--- run-cmake-android.sh
+++ run-cmake-android.sh
@@ -7,8 +7,23 @@ set -x
 NDK_DIRECTORY=/Users/ooandreev/Library/Android/sdk/ndk-bundle
 NDK_ARCH_ABI=arm64-v8a
 #NDK_ARCH_ABI=armeabi-v7a
-NDK_VERSION=23
+NDK_VERSION=24
 NDK_TOOLCHAIN_VERSION=clang
+
+# 1. Recent NDK have broken (?) ld invocation (see e.g.
+#    https://github.com/flutter/flutter/issues/23458), fix by manually passing the LDFLAGS.
+# 2. The linker expects to find libgcc in
+#    toolchains/llvm/prebuilt/darwin-x86_64/lib64/clang/8.0.2/lib/linux/aarch64/ while it is
+#    actually located in toolchains/llvm/prebuilt/darwin-x86_64/lib/gcc/aarch64-linux-android/4.9.x/
+# 3. The linker can't find libatomic, manually add the required directory.
+NDK_LD=`$NDK_DIRECTORY/ndk-which --abi $NDK_ARCH_ABI ld`
+
+# LDFLAGS are the easiest way to pass linker flags to the CMake.
+export LDFLAGS="-fuse-ld=$NDK_LD \
+-L$NDK_DIRECTORY/toolchains/llvm/prebuilt/darwin-x86_64/lib/gcc/aarch64-linux-android/4.9.x \
+-L$NDK_DIRECTORY/toolchains/llvm/prebuilt/darwin-x86_64/lib/gcc/arm-linux-android/4.9.x \
+-L$NDK_DIRECTORY/toolchains/llvm/prebuilt/darwin-x86_64/aarch64-linux-android/lib64 \
+-L$NDK_DIRECTORY/toolchains/llvm/prebuilt/darwin-x86_64/arm-linux-android/lib"
 
 cmake -DCMAKE_SYSTEM_NAME=Android \
   -DCMAKE_ANDROID_NDK=$NDK_DIRECTORY \
